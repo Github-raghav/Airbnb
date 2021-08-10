da3d8b229598ce1d954925854402435dfe50e8aa
Raghav Arora <araghav621@gmail.com>
2021-08-10 17:21:19 +0530

    final demo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports={
     images:{
-        domains:['links.papareact.com',
-        'images.pexels.com'],
+        domains:['links.papareact.com'],
+    },
+    env:{
+        mapbox_key:"pk.eyJ1IjoicmFnaGF2YXJvcmEiLCJhIjoiY2tzNW9pdHBqMnpoMzJxczdjeDh1ZW9uZCJ9.A1BN3EmqbIpDOUdhrjFIJg",
     }
-}
+};
